83034c4faf63f37847e4beb4aa36cee474c2f0d5
= <=>
2023-11-24 13:13:55 -0300

    atu006

--- src/App.vue
+++ src/App.vue
@@ -26,14 +26,90 @@
 </nav>
 
 <div class="retangle">
+  <h2 class="winrate">Win Rate</h2>
+  <div class="progress">
+  <div class="progress-bar bg-success" style="width:65%"></div>
+</div>
+<p class="porce">52,33%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-success" style="width:59%"></div>
+</div>
+<p class="porce">51,90%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-success" style="width:48%"></div>
+</div>
+<p class="porce">51,89%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-success" style="width:30%"></div>
+</div>
+<p class="porce">51,84%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
   
 </div>
 
 <div class="retangle2">
+  <h2 class="winrate">Ban Rate</h2>
+  <div class="progress">
+  <div class="progress-bar bg-danger" style="width:29%"></div>
+</div>
+<p class="porce">29,12%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+  <br>
+  <div class="progress">
+  <div class="progress-bar bg-danger" style="width:16%"></div>
+</div>
+<p class="porce">16,33%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-danger" style="width:14%"></div>
+</div>
+<p class="porce">14,62%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-danger" style="width:13%"></div>
+</div>
+<p class="porce">13,46%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+  
 
 </div>
 
 <div class="retangle3">
+  <h2 class="winrate">Pick Rate</h2>
+  <div class="progress">
+  <div class="progress-bar" style="width:100%"></div>
+</div>
+<p class="porce">52,33%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar" style="width:100%"></div>
+</div>
+<p class="porce">52,33%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+  <br>
+  <div class="progress">
+  <div class="progress-bar" style="width:100%"></div>
+</div>
+<p class="porce">52,33%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+  <br>
+  <div class="progress">
+  <div class="progress-bar" style="width:100%"></div>
+</div>
+<p class="porce">52,33%</p>
+<img  class="img" src="src/assets/Tahm_KenchSquare 1.png" alt="">
+  
+  
+  
 
 </div>
 
@@ -47,6 +123,40 @@
    
 
 <style scoped>
+.img{
+  filter: brightness(2.1);
+  width: 10%;
+  margin-left: 8.5%;
+  margin-top: -18%;
+  border-radius: 20%;
+}
+.porce{
+  margin-left: 63%;
+  margin-top: -6%;
+  color: #000;
+font-family: Farro;
+font-size: 25px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+}
+.progress{
+  width: 35%;
+  height: 5%;
+  margin-left: 23%;
+  margin-top: 20%;
+  
+}
+
+.winrate{
+  margin-left: 3.5%;
+  margin-top: 5%;
+  color: #FFF;
+text-align: center;
+font-family: Manjari;
+font-style: normal;
+line-height: normal;
+}
 .retangle3{
   width: 500px;
 height: 780px;
